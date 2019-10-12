926209bccc5981d676edb0cbcedab95e1fb24b8b
hactar <hactar@cyberkraft.org>
2019-10-13 00:46:38 +0200

    Initial commit

---
+++ content/_index.md
@@ -0,0 +1,5 @@
++++
+title = "Hallo ich bin Henning"
++++
+
+Ich bin ein Software Entwickler aus Basel und Arbeite bei [cyon.ch](https://cyon.ch).
---
+++ themes/dahlheim.ch/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      textColor: {
+        'highlight': '#fa211f'
+      },
+      fontFamily: {
+        'sans': ['work-sans', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif'],
+        'mono': ['"IBM-Plex-Mono"', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace']
+      },
+      maxWidth: {
+        'line-xs': '20em',
+        'line-sm': '25em',
+        'line-lg': '30em',
+        'line-xl': '35em',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
