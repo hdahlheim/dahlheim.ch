5ab0742254e5554e5523a202868391f850cd76cb
Henning Dahlheim <dev@dahlheim.ch>
2023-09-16 16:14:27 +0200

    refactor(projects): update list style

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: watch-css build-css deploy
 watch-css:
-	npx tailwindcss@latest -c ./tailwind.config.js -i ./assets/_src/css/styles.css -o ./assets/css/styles.css --watch
+	tailwind -c ./tailwind.config.js -i ./assets/_src/css/styles.css -o ./assets/css/styles.css --watch
 
 deploy:
 	ansible-playbook -i hosts deploy.yaml
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "dahlheim.ch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+   outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+    devShells.default =
+      pkgs.mkShell {
+        packages = [
+          pkgs.hugo
+          pkgs.nodejs_20
+          pkgs.nodePackages.tailwindcss
+          pkgs.nodePackages.pnpm
+        ];
+      };
+  });
+}
